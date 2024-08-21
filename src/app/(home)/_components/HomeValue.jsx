8a9d7c62cba810e6172956f1a7b60df3df6f3ff8
konstantinrts <k.developer@acceptance.ltd>
2024-08-21 22:43:13 +0300

    All files 07

---
+++ src/app/(home)/_components/HomeValue.jsx
@@ -0,0 +1,79 @@
+"use client";
+import React, { useState } from "react";
+import "@/styles/home/home.scss";
+import Link from "next/link";
+import { fadeInUp } from "@/utils/animations";
+import { motion } from "framer-motion";
+import ArrowRight from "@/icons/slider/ArrowRight";
+
+
+const HomeValue = () => {
+    return (
+        <section className="home-value">
+            <div className="home-value__container _container">
+                <div className="home-value__body">
+                    <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true }}
+                        variants={fadeInUp}
+                        className="home-value__row-01">
+                        <div className="home-value__col-01">
+                            <h2 className="home-value__title">360° VALUE</h2>
+                        </div>
+                        <div className="home-value__col-02">
+                            <Link href="/solutions" className="home-value__link">
+                                Let's Talk Solutions
+                                <ArrowRight />
+                            </Link>
+                        </div>
+                    </motion.div>
+                    <div className="home-value__row-02">
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true }}
+                            variants={fadeInUp}
+                            className="home-value__item">
+                            <div className="label">Digital Transformation</div>
+                            <div className="sublabel">Accelerate Your Digital Growth</div>
+                            <div className="text">Integrating IT and Marketing is essential for staying competitive. We streamline your digital transformation by aligning technology with strategic marketing, ensuring your business adapts quickly and thrives in the digital landscape.</div>
+                        </motion.div>
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true }}
+                            variants={fadeInUp}
+                            className="home-value__item">
+                            <div className="label">Efficiency and Innovation</div>
+                            <div className="sublabel">Optimise and Innovate Your Marketing</div>
+                            <div className="text">Our IT solutions empower your marketing efforts. We enhance your infrastructure to support scalable, data-driven campaigns that maximise impact, reduce costs, and increase speed to market.</div>
+                        </motion.div>
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true }}
+                            variants={fadeInUp}
+                            className="home-value__item">
+                            <div className="label">Customer-Centric Solutions</div>
+                            <div className="sublabel">Deliver Personalised CX</div>
+                            <div className="text">We leverage data, CRM systems, and automation tools to craft customer-centric strategies. Our IT-backed marketing solutions ensure you meet and exceed customer expectations, driving loyalty and growth.</div>
+                        </motion.div>
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true }}
+                            variants={fadeInUp}
+                            className="home-value__item">
+                            <div className="label">Strategic Alignment</div>
+                            <div className="sublabel">Align IT and Marketing for Maximum ROI</div>
+                            <div className="text">We ensure your IT and Marketing strategies align, leading to unified goals and measurable results. Our approach maximises ROI by optimising resources and driving consistent business performance.</div>
+                        </motion.div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default HomeValue;
--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,10 +1,12 @@
 import HomeHero from "./_components/HomeHero";
+import HomeValue from "./_components/HomeValue";
 
 
 export default function Home() {
   return (
     <>
       <HomeHero />
+      <HomeValue />
     </>
   );
 }
