f3531130d35d67ef78e7886e3880d5722632c969
konstantinrts <k.developer@acceptance.ltd>
2024-09-16 21:24:17 +0300

    all files 50

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -6,6 +6,17 @@ import HomeWhat from "./_components/HomeWhat";
 import HomeMarket from "./_components/HomeMarket";
 import HomeLast from "./_components/HomeLast";
 
+export const metadata = {
+  title: "IT and Marketing Solutions for Business Growth | Nexoria",
+  description:
+    "Explore Nexoria's comprehensive IT and marketing solutions designed to drive your business forward. From IT infrastructure to targeted marketing strategies, we optimise growth and efficiency.",
+  openGraph: {
+    title: "IT and Marketing Solutions for Business Growth | Nexoria",
+    description:
+      "Explore Nexoria's comprehensive IT and marketing solutions designed to drive your business forward. From IT infrastructure to targeted marketing strategies, we optimise growth and efficiency.",
+    images: "https://nexoria.ai/images/banner.jpg/",
+  },
+};
 
 export default function Home() {
   return (
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,17 +7,17 @@ import AboutText from "./_components/AboutText";
 import AboutMain from "./_components/AboutMain";
 import AboutBottom from "./_components/AboutBottom";
 
-/* export const metadata = {
-    title: "Contact  Marketing Experts",
+export const metadata = {
+    title: "",
     description:
-        "Have questions or need expert help with your marketing strategy? Reach out to Next Wave Ad for personalised assistance and solutions tailored to your business needs.",
+      "",
     openGraph: {
-        title: "Contact  Marketing Experts",
-        description:
-            "Have questions or need expert help with your marketing strategy? Reach out to Next Wave Ad for personalised assistance and solutions tailored to your business needs.",
-        images: "",
+      title: "",
+      description:
+        "",
+      images: "",
     },
-}; */
+  };
 
 const About = () => {
     return (
